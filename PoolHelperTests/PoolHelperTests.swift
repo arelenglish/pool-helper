d4def006e210c41d38b21c5d90c7cdfe6198111e
Arel English <arelenglish@gmail.com>
2026-08-11 22:44:26 -0400

    Initial Commit

---
+++ PoolHelperTests/PoolHelperTests.swift
@@ -0,0 +1,19 @@
+//
+//  PoolHelperTests.swift
+//  PoolHelperTests
+//
+//  Created by Arel English on 8/11/26.
+//
+
+import Testing
+@testable import PoolHelper
+
+struct PoolHelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
